b66da0eb4fb439000f28b2777cf2de281f1e2c97
Patrick Shoffner <p_shoffner@outlook.com>
2025-08-12 09:38:39 -0700

    Setup Player

---
+++ Assets/Scripts/Player Control/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerType playerType;
+    public Transform playerTransform;
+    public float speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
